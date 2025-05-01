a6299fcad9607a74811acc0b2d80886e3fad1366
Matt Lewis <matt@mplewis.com>
2025-05-01 12:58:56 -0600

    fix: eslint config works in vscode

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,11 @@
-import eslint from '@eslint/js';
-import tseslint from '@typescript-eslint/eslint-plugin';
-import tsparser from '@typescript-eslint/parser';
-import prettier from 'eslint-plugin-prettier';
-import eslintConfigPrettier from 'eslint-config-prettier';
-import globals from 'globals';
+const eslint = require('@eslint/js');
+const tseslint = require('@typescript-eslint/eslint-plugin');
+const tsparser = require('@typescript-eslint/parser');
+const prettier = require('eslint-plugin-prettier');
+const eslintConfigPrettier = require('eslint-config-prettier');
+const globals = require('globals');
 
-export default [
+module.exports = [
 	eslint.configs.recommended,
 	{
 		files: ['**/*.ts'],
@@ -25,6 +25,20 @@ export default [
 			...tseslint.configs.recommended.rules,
 			'prettier/prettier': ['error', { useTabs: true }],
 			'@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+			indent: ['error', 'tab'],
+		},
+	},
+	{
+		files: ['**/*.js'],
+		languageOptions: {
+			parserOptions: {
+				ecmaVersion: 'latest',
+				sourceType: 'module',
+			},
+			globals: { ...globals.node },
+		},
+		rules: {
+			'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
 			indent: ['error', 'tab'],
 		},
 	},
